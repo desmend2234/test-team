cca053b07997c992d2cb23fbddf6be53ab53322e
desmend2234 <143927374+desmend2234@users.noreply.github.com>
2023-11-26 15:11:26 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './layout/header';
+import './pages/5-1_booking.js';
+import './pages/5-1-1_booking.js';
+// console.log('Hello world!!!');
+// console.log('Hello world*2!!!');
+// console.log('成功加入');
